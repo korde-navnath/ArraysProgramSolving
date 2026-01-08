9740dde81632ebe1f264c3dfae71aeacb0f36830
Navnath Korde <navnathk545@gmail.com>
2026-01-08 22:40:31 +0530

    Kids With Gretest Numbers Of Candies Program solved

---
+++ ArraysProgramSolving/KidsWithGretestNumbersOfCandies.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysProgramSolving
+{
+    internal class KidsWithGretestNumbersOfCandies
+    {
+        public static IList<bool> KidsWithGretestNumbersOfCandie(int[] candies, int extraCandies)
+        {
+            int maxCandies = candies.Max();
+            IList<bool> result = new List<bool>();
+            foreach (int candy in candies)
+            {
+                if (candy + extraCandies >= maxCandies)
+                {
+                    result.Add(true);
+                }
+                else
+                {
+                    result.Add(false);
+                }
+            }
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            int[] candies = { 2, 3, 5, 1, 3 };
+            int extraCandies = 3;
+            KidsWithGretestNumbersOfCandie(candies, extraCandies);
+            foreach (var item in KidsWithGretestNumbersOfCandie(candies, extraCandies))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
