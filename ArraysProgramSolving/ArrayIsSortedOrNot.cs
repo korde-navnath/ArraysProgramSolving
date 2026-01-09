393e396bdbeb41449101678cd7e1141a317ba7a9
Navnath Korde <navnathk545@gmail.com>
2026-01-09 15:01:21 +0530

    Problem Solved

---
+++ ArraysProgramSolving/ArrayIsSortedOrNot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysProgramSolving
+{
+    internal class ArrayIsSortedOrNot
+    {
+        public static bool IsSortedOrNot(int[] arr, int n)
+        {
+            for(int i = 1; i < n; i++)
+            {
+                if (arr[i] >= arr[i - 1])
+                {
+
+                }
+                else return false;
+            }
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int n = arr.Length;
+            bool answer = IsSortedOrNot(arr, n);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ ArraysProgramSolving/RemoveDuplicates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysProgramSolving
+{
+    internal class RemoveDuplicates
+    {
+        public static int RemoveDuplicatesAndReturnNewArrayLength(int[] arr, int n)
+        {
+            int i = 0;
+            for(int j = 1; j < n; j++)
+            {
+                if (arr[i] != arr[j])
+                {
+                    arr[i + 1] = arr[j];
+                    i++;
+                }
+            }
+            return i + 1;
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+            int n = arr.Length;
+            int printResult = RemoveDuplicatesAndReturnNewArrayLength(arr, n);
+            Console.WriteLine(printResult);
+        }
+    }
+}
